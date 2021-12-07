e3c52fb93070b555db2c62c558472b6c9248e995
lyudmila50 <81586606+lyudmila50@users.noreply.github.com>
2021-12-07 16:51:22 -0300

    Add files via upload

--- script.js
+++ script.js
@@ -1,4 +1,10 @@
 
 
-
+const aplication = document.querySelector('.container')
+function traer(){
+    fetch('https://api.covid19api.com/summary')
+    .then(response => response.json())
+    .then(json => console.log(json))
+   
+  }
 
